da8f4346234acbebdb00391753c18b8e81c2c3e1
Mark Vainomaa <mikroskeem@mikroskeem.eu>
2018-03-29 12:26:59 +0300

    Improve ProvidersLibBinding initialization

--- src/main/java/eu/mikroskeem/debug/bukkitgroovy/groovybindings/ProvidersLibBinding.java
+++ src/main/java/eu/mikroskeem/debug/bukkitgroovy/groovybindings/ProvidersLibBinding.java
@@ -34,47 +34,64 @@ import eu.mikroskeem.providerslib.api.Providers;
 import eu.mikroskeem.providerslib.api.Spawnpoint;
 import eu.mikroskeem.providerslib.api.Title;
 import eu.mikroskeem.providerslib.api.Vanish;
-import eu.mikroskeem.shuriken.reflect.Reflect;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.RegisteredServiceProvider;
 
 import java.util.HashMap;
+import java.util.Map;
 
 import static com.google.common.base.Preconditions.checkNotNull;
 
 public class ProvidersLibBinding implements BindingProvider {
-    private HashMap<String, Object> cachedBindings;
-    @Inject private Actionbar actionbar;
-    @Inject private Chat chat;
-    @Inject private Permissions permissions;
-    @Inject private Spawnpoint spawnpoint;
-    @Inject private Title title;
-    @Inject private Vanish vanish;
+    private HashMap<String, Object> bindings = new HashMap<>();
+
+    @Inject
+    private void setActionbar(Actionbar actionbar) {
+        bindings.put("actionbar", actionbar);
+    }
+
+    @Inject
+    private void setChat(Chat chat) {
+        bindings.put("chat", chat);
+    }
+
+    @Inject
+    private void setPermissions(Permissions permissions) {
+        bindings.put("permissions", permissions);
+    }
+
+    @Inject
+    public void setSpawnpoint(Spawnpoint spawnpoint) {
+        bindings.put("spawnpoint", spawnpoint);
+    }
+
+    @Inject
+    public void setTitle(Title title) {
+        bindings.put("title", title);
+    }
+
+    @Inject
+    public void setVanish(Vanish vanish) {
+        bindings.put("vanish", vanish);
+    }
 
     @Override
-    public HashMap<String, Object> getBindings() {
-        if(cachedBindings == null) {
-            cachedBindings = new HashMap<String, Object>() {{
-                Reflect.wrapInstance(ProvidersLibBinding.this).getFields().forEach(field -> {
-                    if(field.getAnnotation(Inject.class).isPresent()) {
-                        put(field.getName(), field.read());
-                    }
-                });
-            }};
-        }
-        return cachedBindings;
+    public Map<String, Object> getBindings() {
+        return bindings;
     }
 
     public static class Initializer {
         private static ProvidersLibBinding cachedInstance = null;
         public static ProvidersLibBinding getInstance() {
-            if(cachedInstance == null) {
-                RegisteredServiceProvider<Providers> providers = Bukkit.getServer().getServicesManager()
-                        .getRegistration(Providers.class);
-                Injector injector = checkNotNull(providers.getProvider()).getInjector();
-                cachedInstance = injector.getInstance(ProvidersLibBinding.class);
+            synchronized(Initializer.class) {
+                if(cachedInstance == null) {
+                    RegisteredServiceProvider<Providers> providers = Bukkit.getServer().getServicesManager()
+                            .getRegistration(Providers.class);
+                    Injector injector = checkNotNull(providers.getProvider()).getInjector();
+                    cachedInstance = injector.getInstance(ProvidersLibBinding.class);
+                }
+                return cachedInstance;
             }
-            return cachedInstance;
         }
     }
 }
